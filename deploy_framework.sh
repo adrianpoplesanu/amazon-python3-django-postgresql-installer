4ae8ac9c887d9a1ef69ef6e20c6926362de53a1c
Adrian Poplesanu <adrian.poplesanu@yahoo.com>
2020-11-21 19:03:27 +0000

    added installation for gcc openssl-devel bzip2-devel libffi-devel

--- deploy_framework.sh
+++ deploy_framework.sh
@@ -1,10 +1,15 @@
-echo "    _      _     _                  ";
-echo "   /_\  __| |_ _(_)__ _ _ _ _  _ ___";
-echo "  / _ \/ _\` | '_| / _\` | ' \ || (_-<";
-echo " /_/ \_\__,_|_| |_\__,_|_||_\_,_/__/";
-echo "                                    ";
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+LIGHT_PURPLE='\033[1;35m'
+NC='\033[0m' # No Color
 
-echo "Adrianus installer 2o2o" ;
+echo -e "${LIGHT_PURPLE}    _      _     _                  ${NC}";
+echo -e "${LIGHT_PURPLE}   /_\  __| |_ _(_)__ _ _ _ _  _ ___${NC}";
+echo -e "${LIGHT_PURPLE}  / _ \/ _\` | '_| / _\` | ' \ || (_-<${NC}";
+echo -e "${LIGHT_PURPLE} /_/ \_\__,_|_| |_\__,_|_||_\_,_/__/${NC}";
+echo -e "${LIGHT_PURPLE}                    november 2o2o   ${NC}";
+
+echo "Adrianus Installer running" ;
 echo "This will install a Python 3.9 distribution, Django, PostgreSQL and create and deploy a new project on an Amazon Linux 2 instance"
 
 echo ;
@@ -13,5 +18,10 @@ echo "WARNING: Prior to running this command make sure you've ran:" ;
 echo "sudo yum update" ;
 echo ;
 
-#python tests/test_config_parser.py ;
 python load_configs.py ;
+
+echo "Installing gcc openssl-devel bzip2-devel libffi-devel" ;
+
+sudo yum install gcc openssl-devel bzip2-devel libffi-devel
+
+echo -e "${GREEN}gcc openssl-devel bzip2-devel libffi-devel installed!${NC}" ;
